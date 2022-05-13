d4d007c151cc460f330bdfbb800f254c79fc7c49
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-14 00:55:08 +0200

    grid random rotate

--- src/Components/PhotoGridItem.jsx
+++ src/Components/PhotoGridItem.jsx
@@ -6,12 +6,37 @@ export const PhotoGridItem = ({ size, id, title, category }) => {
   let url = `https://picsum.photos/id/${id}/1080/768`;
   url = `https://picsum.photos/1080/768?random=${id}`;
 
+  // random between 0 and 2
+  const random = Math.floor(Math.random() * 3);
+
+  // random 1 or 2 
+  const random2 = Math.floor(Math.random() * 2) + 1;
+
+  // random 2 or 3 
+  const random3 = Math.floor(Math.random() * 3) + 2;
+
+  const twothree = [2, 3]
+  const twothreeRandom = twothree[Math.floor(Math.random() * twothree.length)];
+
+  // console.log('random', random);
+
+  const plusmin = ['', '-'];
+  // get random array element
+  const plusminRandom = plusmin[Math.floor(Math.random() * plusmin.length)];
+
+  // console.log('plusminRandom', plusminRandom);
+
+  const rotate = `${plusminRandom}rotate-2`;
+
+  // console.log(rotate)
+
+
   return (
     // <div className={size + ' shadow-sm shadow-black cursor-pointer'}>
     <Link
       className={
         size +
-        ' shadow-sm shadow-black cursor-pointer rounded ease-in-out duration-300 hover:scale-110 hover:-rotate-2 hover:translate-x-2 hover:translate-y-2'
+        ` shadow-sm shadow-black cursor-pointer rounded ease-in-out duration-300 hover:scale-110 hover:${rotate} hover:translate-x-2 hover:translate-y-2 hover:z-50`
       }
       to={`/photos/${category}/${helpers.toSeoFriendly(title)}`}
     >
