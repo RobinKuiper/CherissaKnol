8d2baa7326d516230c470a8272ef4425b0281645
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-14 09:29:02 +0200

    Rotate

--- src/Components/PhotoGridItem.jsx
+++ src/Components/PhotoGridItem.jsx
@@ -4,24 +4,39 @@ import { helpers } from '../utils';
 import { SuspenseImg } from './SuspenseImg';
 
 export const PhotoGridItem = ({ size, id, title, category }) => {
-  const [rotate, setRotate] = useState()
+  const [transform, setTransform] = useState('0deg scale(1)');
 
   let url = `https://picsum.photos/id/${id}/1080/768`;
   url = `https://picsum.photos/1080/768?random=${id}`;
 
-  useEffect(() => {
-    // random between 0 and 2
-    const random = Math.floor(Math.random() * 3);
+  const changeTransform = () => {
+    // random random 1 or 2
+    const random = Math.floor(Math.random() * 2) + 1;
+    // random minus or nothing
+    const minus = Math.floor(Math.random() * 2) === 0 ? '-' : '';
 
-    setRotate(random === 0 ? '-rotate-2' : 'rotate-2');
-  }, [])
+    const degrees = `${minus}${random}deg`;
+    const multiplier = size === '' ? 1.3 : 1.1;
+
+    setTransform(`rotate(${degrees}) scale(${multiplier})`);
+  };
+
+  const resetTransform = () => {
+    setTransform('rotate(0deg) scale(1)');
+  };
 
   return (
     <Link
       className={
         size +
-        ` shadow-sm shadow-black cursor-pointer rounded ease-in-out duration-300 hover:shadow-2xl hover:shadow-gray-900 hover:scale-110 hover:${rotate} hover:z-50`
+        ` shadow-sm shadow-black cursor-pointer rounded ease-in-out duration-300 hover:shadow-2xl hover:shadow-gray-900 hover:z-50`
       }
+      style={{
+        transform,
+      }}
+      onMouseEnter={changeTransform}
+      onMouseLeave={resetTransform}
+      onMouseOut={() => resetTransform}
       to={`/photos/${category}/${helpers.toSeoFriendly(title)}`}
     >
       <SuspenseImg
