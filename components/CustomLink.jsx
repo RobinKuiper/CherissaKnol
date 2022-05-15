c21e5374f671d5038b5c9a53669915f602336353
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-15 10:11:15 +0200

    Different link on exported

--- components/CustomLink.jsx
+++ components/CustomLink.jsx
@@ -3,16 +3,7 @@ import Link from 'next/link';
 export const CustomLink = (props) => {
   const { children, href, ...attr } = props;
 
-  console.log('NODE ENV:', process.env.NODE_ENV);
-
-  // const path =
-  //   process.env.NODE_ENV !== 'development'
-  //     ? href === '/'
-  //       ? '/'
-  //       : `${href}.html`
-  //     : href;
-
-  const path = href;
+  const path = process.env.EXPORTED && href !== '/' ? `${href}.html` : href;
 
   return (
     <Link href={path} {...attr}>
