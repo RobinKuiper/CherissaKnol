756954954e838c58f484336a77f2b2771ca10c16
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-15 14:13:31 +0200

    Fix category link to full li

--- components/CategoryMenuItem.jsx
+++ components/CategoryMenuItem.jsx
@@ -5,26 +5,19 @@ export const CategoryMenuItem = ({ path, title, toggleCollapse }) => {
   const { query } = useRouter();
 
   return (
-    <li
-      className={`
-      pl-5 p-2 ease-in-out duration-500 hover:pl-6
+    <CustomLink href={path}>
+      <li
+        className={`
+      pl-5 p-2 ease-in-out duration-500 hover:pl-6 cursor-pointer
       ${
         query.category && query.category === title.toLowerCase()
           ? 'bg-orange-300'
           : ''
       } hover:bg-orange-300`}
-      onClick={toggleCollapse}
-    >
-      <CustomLink
-        href={path}
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'block',
-        }}
+        onClick={toggleCollapse}
       >
         {title}
-      </CustomLink>
-    </li>
+      </li>
+    </CustomLink>
   );
 };
