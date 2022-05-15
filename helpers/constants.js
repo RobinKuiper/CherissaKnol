19a29d15700c4b8bdd409a722edc3c8a93bd3bff
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-15 18:51:44 +0200

    ..

---
+++ helpers/constants.js
@@ -0,0 +1,11 @@
+const FB_URL = 'https://www.facebook.com/cherissa.knol'
+const IG_URL = 'https://www.instagram.com/cherissaknol/'
+const MAIL = 'cherissa@example.com'
+
+const constants = {
+  FB_URL,
+  IG_URL,
+  MAIL
+};
+
+export default constants;
