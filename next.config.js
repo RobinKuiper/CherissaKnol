43ca8a08abb4c0eb8c6e70a07eec0831141bf57e
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-16 12:04:54 +0200

    Added webpack analyzing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
   images: {
     domains: ['i.imgur.com', 'picsum.photos'], //TODO: Remove for production
-  }
+  },
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   if (process.env.ANALYZE) {
+  //     config.plugins.push(
+  //       new BundleAnalyzerPlugin({
+  //         analyzerMode: 'server',
+  //         analyzerPort: isServer ? 8888 : 8889,
+  //         openAnalyzer: true,
+  //       })
+  //     )
+  //   }
+  //   return config
+  // },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
