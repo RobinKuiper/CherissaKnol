d502e7feecfb15f89fc11d9a9526fd97c0554e26
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-18 16:57:55 +0200

    updates

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs';
+import prisma from '../src/lib/prisma';
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+// import bcrypt from 'bcryptjs';
+
+async function main() {
+  const salt = await bcrypt.genSalt(10);
+
+  await prisma.user.upsert({
+    where: { email: process.env.ADMIN_EMAIL },
+    update: {},
+    create: {
+      email: process.env.ADMIN_EMAIL,
+      username: process.env.ADMIN_USERNAME,
+      password: await bcrypt.hash(process.env.ADMIN_PASSWORD, salt),
+    },
+  })
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
