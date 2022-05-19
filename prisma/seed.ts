2491cd81363bdc5fc9668758e1130ed12a73fe0b
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-19 23:46:47 +0200

    Add Cherissa to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,5 @@
 import bcrypt from 'bcryptjs';
 import prisma from '../src/lib/prisma';
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient()
-// import bcrypt from 'bcryptjs';
 
 async function main() {
   const salt = await bcrypt.genSalt(10);
@@ -14,6 +11,16 @@ async function main() {
       email: process.env.ADMIN_EMAIL,
       username: process.env.ADMIN_USERNAME,
       password: await bcrypt.hash(process.env.ADMIN_PASSWORD, salt),
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: process.env.CHERISSA_EMAIL },
+    update: {},
+    create: {
+      email: process.env.CHERISSA_EMAIL,
+      username: process.env.CHERISSA_USERNAME,
+      password: await bcrypt.hash(process.env.CHERISSA_PASSWORD, salt),
     },
   })
 }
