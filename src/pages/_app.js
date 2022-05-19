e69b3bbe8815fe789f7bc016e66300782c971376
Robin Kuiper <robingjkuiper@gmail.com>
2022-05-19 11:05:33 +0200

    Page loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,25 @@ import '../styles/globals.css'
 import '@fontsource/roboto';
 import "@fontsource/bebas-neue"
 import { AnimatePresence, AnimateSharedLayout } from 'framer-motion';
-import { useRouter } from 'next/router';
+import { Router, useRouter } from 'next/router';
 import { SessionProvider } from 'next-auth/react';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+NProgress.configure({
+  template: ' \
+  <div class="custom_bar" role="bar"> \
+    <div class="custom_peg"></div> \
+  </div> \
+  <div class="spinner" role="spinner"> \
+    <div class="spinner-icon"></div> \
+  </div>',
+  showSpinner: false
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter();
